22788099899b58d4d70182ffa9a552febaf36b66
mmanyuat <mmanyuat201@gmail.com>
2024-11-14 18:40:11 +0000

    API

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""""Auth module for managing api authenication"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Template for API authenication management"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """return if path is none"""
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') and path == excluded_path:
+                return False
+
+        return True
+    
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request.
+        Currently returns None as a placeholder.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user from the request.
+        Currently returns None as a placeholder.
+        """
+        return None
