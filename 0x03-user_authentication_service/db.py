562447341fcda9d464d3958f1498c0461be9f911
mmanyuat <mmanyuat201@gmail.com>
2024-11-20 17:00:34 +0000

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -13,17 +15,15 @@ class DB:
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -44,3 +44,28 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        method that matches the provided keyword arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering the query.
+
+        Returns:
+            User: The first user object matching the criteria.
+
+        Raises:
+            NoResultFound: If no user is found matching the criteria.
+            InvalidRequestError: If the query arguments are invalid.
+        """
+        if not kwargs:
+            raise InvalidRequestError("No arguments provided")
+        try:
+            query = self._session.query(User).filter_by(**kwargs)
+            user = query.one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found matching the criteria")
+        except Exception as e:
+            raise InvalidRequestError(f"Invalid query: {str(e)}")
